b2a0d078c9996a7f0845982a3ebc0a39dbaf2571
Phuc <phuc@MacBook-Pro-anhphucne.local>
2024-09-30 22:38:09 +0200

    Hometask2

--- Java-hometask2/src/Car.java
+++ Java-hometask2/src/Car.java
@@ -1,9 +1,67 @@
+import java.util.ArrayList;
+import java.util.List;
 
-public class Car {
+class Car {
+    private int id;
+    private String make;
+    private String model;
+    private int yearOfManufacture;
+    private String color;
+    private double price;
+    private String registrationNumber;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    // Constructor
+    public Car(int id, String make, String model, int yearOfManufacture, String color, double price, String registrationNumber) {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.yearOfManufacture = yearOfManufacture;
+        this.color = color;
+        this.price = price;
+        this.registrationNumber = registrationNumber;
+    }
 
-	}
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYearOfManufacture() {
+        return yearOfManufacture;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", yearOfManufacture=" + yearOfManufacture +
+                ", color='" + color + '\'' +
+                ", price=" + price +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                '}';
+    }
+
 
 }
